41291f47617e593db1dfbc949044e3fb81b7b088
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-05-20 13:00:03 +0200

    :recycle: Refactored game code

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,12 +7,87 @@ import GameField from "./GameField.vue";
 
 const state = ref<{
   players: Player[];
+  game: {
+    field: string[];
+    isRunning: boolean;
+    currentPlayerO: boolean;
+    currentTurn: number;
+  };
 }>({
   players: [new Player(), new Player()],
+  game: {
+    field: ["", "", "", "", "", "", "", "", ""],
+    isRunning: true,
+    currentPlayerO: false,
+    currentTurn: 0,
+  },
 });
 
 const playerNameChange = (name: string, i: number) => {
   state.value.players[i].name = name;
+};
+
+const fieldClick = (i: number) => {
+  if (state.value.game.currentPlayerO) {
+    state.value.game.field[i] = "O";
+  }
+  if (!state.value.game.currentPlayerO) {
+    state.value.game.field[i] = "X";
+  }
+  endTurn();
+};
+
+const startTurn = () => {
+  state.value.game.isRunning = true;
+  state.value.game.currentPlayerO = !state.value.game.currentPlayerO;
+  state.value.game.currentTurn += 1;
+};
+
+const endTurn = () => {
+  state.value.game.isRunning = false;
+  checkWin();
+};
+
+const checkWin = () => {
+  if (state.value.game.currentTurn < 4) return startTurn();
+
+  const currentPlayer: string = state.value.game.currentPlayerO ? "O" : "X";
+
+  if (
+    (state.value.game.field[0] === currentPlayer &&
+      state.value.game.field[1] === currentPlayer &&
+      state.value.game.field[2] === currentPlayer) ||
+    (state.value.game.field[3] === currentPlayer &&
+      state.value.game.field[4] === currentPlayer &&
+      state.value.game.field[5] === currentPlayer) ||
+    (state.value.game.field[6] === currentPlayer &&
+      state.value.game.field[7] === currentPlayer &&
+      state.value.game.field[8] === currentPlayer) ||
+    (state.value.game.field[0] === currentPlayer &&
+      state.value.game.field[3] === currentPlayer &&
+      state.value.game.field[6] === currentPlayer) ||
+    (state.value.game.field[1] === currentPlayer &&
+      state.value.game.field[4] === currentPlayer &&
+      state.value.game.field[7] === currentPlayer) ||
+    (state.value.game.field[2] === currentPlayer &&
+      state.value.game.field[5] === currentPlayer &&
+      state.value.game.field[8] === currentPlayer) ||
+    (state.value.game.field[0] === currentPlayer &&
+      state.value.game.field[4] === currentPlayer &&
+      state.value.game.field[8] === currentPlayer) ||
+    (state.value.game.field[2] === currentPlayer &&
+      state.value.game.field[4] === currentPlayer &&
+      state.value.game.field[6] === currentPlayer)
+  ) {
+    return triggerWin();
+  }
+
+  startTurn();
+};
+
+const triggerWin = () => {
+  state.value.game.isRunning = false;
+  console.log("WIN!");
 };
 </script>
 
@@ -23,7 +98,13 @@ const playerNameChange = (name: string, i: number) => {
   </ul>
   <NameEntry :players="state.players" @name-change="playerNameChange" />
   <Scoreboard :players="state.players" />
-  <GameField />
+  <GameField
+    :game-state="state.game"
+    @field-click="
+      (i) => {
+        fieldClick(i);
+      }
+    " />
 </template>
 
 <style scoped></style>
--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -1,77 +1,16 @@
 <script setup lang="ts">
-import { ref } from "vue";
+defineProps<{
+  gameState: {
+    field: string[];
+    isRunning: boolean;
+    currentPlayerO: boolean;
+    currentTurn: number;
+  };
+}>();
 
-const gameState = ref<{
-  field: string[];
-  isRunning: boolean;
-  currentPlayerO: boolean;
-  currentTurn: number;
-}>({
-  field: ["", "", "", "", "", "", "", "", ""],
-  isRunning: true,
-  currentPlayerO: false,
-  currentTurn: 0,
-});
-
-const handleClick = (i: number) => {
-  if (gameState.value.currentPlayerO) {
-    gameState.value.field[i] = "O";
-  }
-  if (!gameState.value.currentPlayerO) {
-    gameState.value.field[i] = "X";
-  }
-  endTurn();
-};
-
-const endTurn = () => {
-  gameState.value.isRunning = false;
-  checkWin();
-  gameState.value.currentPlayerO = !gameState.value.currentPlayerO;
-  startTurn();
-};
-
-const startTurn = () => {
-  gameState.value.currentTurn += 1;
-  gameState.value.isRunning = true;
-};
-
-const checkWin = () => {
-  if (gameState.value.currentTurn < 4) return;
-  const currentPlayer: string = gameState.value.currentPlayerO ? "O" : "X";
-
-  if (
-    (gameState.value.field[0] === currentPlayer &&
-      gameState.value.field[1] === currentPlayer &&
-      gameState.value.field[2] === currentPlayer) ||
-    (gameState.value.field[3] === currentPlayer &&
-      gameState.value.field[4] === currentPlayer &&
-      gameState.value.field[5] === currentPlayer) ||
-    (gameState.value.field[6] === currentPlayer &&
-      gameState.value.field[7] === currentPlayer &&
-      gameState.value.field[8] === currentPlayer) ||
-    (gameState.value.field[0] === currentPlayer &&
-      gameState.value.field[3] === currentPlayer &&
-      gameState.value.field[6] === currentPlayer) ||
-    (gameState.value.field[1] === currentPlayer &&
-      gameState.value.field[4] === currentPlayer &&
-      gameState.value.field[7] === currentPlayer) ||
-    (gameState.value.field[2] === currentPlayer &&
-      gameState.value.field[5] === currentPlayer &&
-      gameState.value.field[8] === currentPlayer) ||
-    (gameState.value.field[0] === currentPlayer &&
-      gameState.value.field[4] === currentPlayer &&
-      gameState.value.field[8] === currentPlayer) ||
-    (gameState.value.field[2] === currentPlayer &&
-      gameState.value.field[4] === currentPlayer &&
-      gameState.value.field[6] === currentPlayer)
-  ) {
-    return triggerWin();
-  }
-};
-
-const triggerWin = () => {
-  console.log("WIN!");
-};
+defineEmits<{
+  (e: "fieldClick", i: number): void;
+}>();
 </script>
 
 <template>
@@ -82,7 +21,7 @@ const triggerWin = () => {
       class="gamefield__btn"
       v-for="(column, i) in gameState.field"
       type="button"
-      @click="() => handleClick(i)"
+      @click="() => $emit('fieldClick', i)"
       :disabled="!gameState.isRunning || column !== ''">
       {{ i + ", " }} {{ column }}
     </button>
