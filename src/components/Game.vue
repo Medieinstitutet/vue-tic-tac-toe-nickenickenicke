b20ee488df7fcde1a2a640d5729cb76f991fb475
nickenickenicke <144111731+nickenickenicke@users.noreply.github.com>
2024-05-20 13:35:38 +0200

    :art: More game logic. Added a button to start a new round.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { Player } from "../models/Player";
+import { IState } from "../models/IState";
 import NameEntry from "./NameEntry.vue";
 import Scoreboard from "./Scoreboard.vue";
 import GameField from "./GameField.vue";
-import { IState } from "../models/IState";
 
 const state = ref<IState>({
   players: [new Player(), new Player()],
@@ -15,25 +15,20 @@ const state = ref<IState>({
     currentTurn: 0,
   },
 });
+
 const LOCAL_STORAGE_KEY: string = "ticTacStorage";
 
 if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
   state.value = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "");
 }
 
+const saveState = () => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state.value));
+};
+
 const playerNameChange = (name: string, i: number) => {
   state.value.players[i].name = name;
   saveState();
-};
-
-const fieldClick = (i: number) => {
-  if (state.value.game.currentPlayerO) {
-    state.value.game.field[i] = "O";
-  }
-  if (!state.value.game.currentPlayerO) {
-    state.value.game.field[i] = "X";
-  }
-  endTurn();
 };
 
 const startTurn = () => {
@@ -46,6 +41,16 @@ const startTurn = () => {
 const endTurn = () => {
   state.value.game.isRunning = false;
   checkWin();
+};
+
+const fieldClick = (i: number) => {
+  if (state.value.game.currentPlayerO) {
+    state.value.game.field[i] = "O";
+  }
+  if (!state.value.game.currentPlayerO) {
+    state.value.game.field[i] = "X";
+  }
+  endTurn();
 };
 
 const checkWin = () => {
@@ -81,6 +86,7 @@ const checkWin = () => {
   ) {
     return triggerWin();
   }
+  if (state.value.game.currentTurn === 9) return triggerDraw();
 
   startTurn();
 };
@@ -95,8 +101,17 @@ const triggerWin = () => {
   saveState();
 };
 
-const saveState = () => {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state.value));
+const triggerDraw = () => {
+  console.log("DRAW!");
+  state.value.game.isRunning = false;
+  saveState();
+};
+
+const startNewRound = () => {
+  state.value.game.currentPlayerO = Math.random() < 0.5;
+  state.value.game.currentTurn = 0;
+  state.value.game.field = ["", "", "", "", "", "", "", "", ""];
+  startTurn();
 };
 </script>
 
@@ -114,6 +129,9 @@ const saveState = () => {
         fieldClick(i);
       }
     " />
+  <button @click.prevent="startNewRound" :disabled="state.game.isRunning">
+    AGAIN!
+  </button>
 </template>
 
 <style scoped></style>
